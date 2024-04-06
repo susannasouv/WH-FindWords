1c55327163ea4dbca00a59734b183b78b04f2bc6
Susanna Souv <susanna.souv@gmail.com>
2024-04-06 13:47:59 -0700

    Add logic to find words that can be formed by letters in input string + comments

--- index.ts
+++ index.ts
@@ -1,5 +1,45 @@
+function isWordConstructable(word: string, inputMap: Map<string, number>): boolean {
+  return word.split('').every((letter) => {
+    if (!inputMap.has(letter)) {
+      // the letter either does not exist or has been used up
+      // the word is definitely unconstructable with the input string letters
+      // so we can break out
+      return false;
+    }
+    const letterCount = inputMap.get(letter) || 0
+    if (letterCount > 1) {
+      // decrement corresponding letter count to indicate usage
+      inputMap.set(letter, letterCount - 1);
+    } else {
+      // last usage of the letter, so we should remove it from the map
+      inputMap.delete(letter);
+    }
+    // the letter existed, so we can continue
+    return true;
+  })
+}
+
 function findWords(inputString: string, dictionary: string[]): string[] {
   const foundWords: string[] = [];
+
+  // map each letter to the number of occurrences
+  const inputMap = new Map<string, number>();
+  inputString.split('').forEach(letter => {
+    const letterCount = inputMap.get(letter) || 0
+    inputMap.set(letter, letterCount + 1)
+  })
+
+  dictionary.forEach(word => {
+    if (word.length > inputString.length) {
+      // if the word is longer than the input string, the word definitely cannot be
+      // formed by the letters in the input string, so we can move on to the next word
+      return;
+    }
+    const inputMapCopy = new Map(inputMap);
+    if (isWordConstructable(word, inputMapCopy)) {
+      foundWords.push(word);
+    }
+  })
   return foundWords;
 }
 
